1b0cf31f786e6552043d2f024dd71999968bb672
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-07-21 01:17:47 +0300

    Add files via upload

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -2,7 +2,6 @@ import express from "express";
 const productRouter = express.Router();
 import {
   getProducts,
-  getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
@@ -13,7 +12,6 @@ import { protect } from "../middlewares/authMiddleware.js";
 productRouter.route("/").get(protect, getProducts).post(protect, createProduct);
 productRouter
   .route("/:id")
-  .get(protect, getProductById)
   .patch(protect, updateProduct)
   .delete(protect, deleteProduct);
 productRouter.route("/barcode/:barcode").get(protect, getProductByBarcode);
